52c9f600cfc58a312e423001d0fcbe2c385a8e13
Fellipe G <Fellipe G@192.168.1.16>
2016-12-11 14:32:14 -0200

    dando uma limpadinha e tals

--- thriftdemo/src/main/java/client/view/ClientInitialization.java
+++ thriftdemo/src/main/java/client/view/ClientInitialization.java
@@ -11,12 +11,12 @@ import client.controller.ThriftClient;
  *
  * @author fellipegurgel
  */
-public class ClientView extends javax.swing.JFrame {
+public class ClientInitialization extends javax.swing.JFrame {
 
     /**
      * Creates new form ClientView
      */
-    public ClientView() {
+    public ClientInitialization() {
         initComponents();
     }
 
@@ -172,20 +172,21 @@ public class ClientView extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ClientView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientInitialization.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ClientView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientInitialization.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ClientView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientInitialization.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ClientView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClientInitialization.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ClientView().setVisible(true);
+                new ClientInitialization().setVisible(true);
             }
         });
     }
